302c1f762a3f4ec3cc3d36b1789058af8fd17326
Little House Computer <littlehousecomputer@Littles-MacBook-Pro.local>
2026-06-22 12:13:00 -0600

    Change headline to Keaton's EPIC App

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Keaton's App</h1>
+          <h1>Keaton's EPIC App</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
